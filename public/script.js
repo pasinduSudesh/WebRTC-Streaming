67bbe4e98c09d40eab7d3d2c07f94ad021fd1824
pasinduSudesh <sudeshdilshan.17@cse.mrt.ac.lk>
2020-10-23 16:33:51 +0530

    aa

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, { 
-  host: 'web-rtc-streaming.herokuapp.com',
+  host: '/',
   secure: true,
   port: '443'
 })
